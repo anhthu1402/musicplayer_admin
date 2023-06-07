c6941ad7cf5d80f6a00643a42a7e975e636153cd
anhthu1402 <20520792@gm.uit.edu,vn>
2023-06-07 22:02:12 +0700

    nothing

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,38 +1,35 @@
-import "../styles/chart.css"
-import {
-    DollarCircleOutlined,
-    ShoppingCartOutlined,
-    ShoppingOutlined,
-    UserOutlined,
-  } from "@ant-design/icons";
+import "../styles/chart.css";
+// import {
+//     DollarCircleOutlined,
+//     ShoppingCartOutlined,
+//     ShoppingOutlined,
+//     UserOutlined,
+//   } from "@ant-design/icons";
 import { Card, Space, Statistic, Table, Typography } from "antd";
 import {
-    LineChart,
-    Line,
-    XAxis,
-    CartesianGrid,
-    Tooltip,
-    ResponsiveContainer,
-  } from "recharts";
-  import {
-    AreaChart,
-    Area,
-  } from "recharts";
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { AreaChart, Area } from "recharts";
 import { UsersData } from "./UserData";
 function DashboardCard({ title, value, icon }) {
-    return (
-      <Card>
-        <Space direction="horizontal">
-          {icon}
-          <Statistic title={title} value={value} />
-        </Space>
-      </Card>
-    );
-  }
-function Chart({ title, data, name, dataKey, grid }){
-    return (
-        <div>
-            {/* <Space direction="horizontal">
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
+function Chart({ title, data, name, dataKey, grid }) {
+  return (
+    <div>
+      {/* <Space direction="horizontal">
                 <DashboardCard
                 icon={
                     <ShoppingCartOutlined
@@ -94,25 +91,28 @@ function Chart({ title, data, name, dataKey, grid }){
                 value={revenue}
                 />
             </Space> */}
-            <div className="chart">
-            <h3 className="chartTitle">{title}</h3>
-            <ResponsiveContainer width="100%" aspect={2/1}>
-                <LineChart data={data} >
-                    <XAxis dataKey={name} stroke="#5550bd" />
-                    <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
-                    <Tooltip />
-                    <Line type="monotone" dataKey={dataKey} stroke="#483D88" strokeDasharray="3 4 5 2"  />
-                    {/* <Line type="monotone" dataKey={dataKey} stroke="#483D88" /> */}
-                    {/* {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 100000" />} */}
-
-                </LineChart>
-            </ResponsiveContainer>
-        </div>
-        </div>
-        
-    )
+      <div className="chart">
+        <h3 className="chartTitle">{title}</h3>
+        <ResponsiveContainer width="100%" aspect={2 / 1}>
+          <LineChart data={data}>
+            <XAxis dataKey={name} stroke="#5550bd" />
+            <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey={dataKey}
+              stroke="#483D88"
+              strokeDasharray="3 4 5 2"
+            />
+            {/* <Line type="monotone" dataKey={dataKey} stroke="#483D88" /> */}
+            {/* {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 100000" />} */}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 }
-export default Chart
+export default Chart;
 
 // const Chart = ({ aspect, title, dataKey, data, name }) => {
 //     return (
@@ -129,7 +129,7 @@ export default Chart
 //               <linearGradient id={dataKey} x1="0" y1="0" x2="0" y2="1">
 //                 <stop offset="5%" stopColor="#8884d8" stopOpacity={0.5} />
 //                 <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-               
+
 //               </linearGradient>
 //             </defs>
 //             <XAxis dataKey={name} stroke="gray"  />
@@ -149,5 +149,5 @@ export default Chart
 //       </div>
 //     );
 //   };
-  
+
 //   export default Chart;
